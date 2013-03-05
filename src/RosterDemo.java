ded5f40d60e327e91380e063f76ce69b8971d759
uberjoel <joel_kurian@yahoo.com>
2013-03-05 18:03:53 +0530

    RosterDemo commit

---
+++ src/RosterDemo.java
@@ -0,0 +1,39 @@
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
+public class RosterDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Create XMPP connection to gmail.com server
+		XMPPConnection connection = new XMPPConnection("gmail.com");
+
+		try {
+			// Connect
+			connection.connect();
+
+			// Login with appropriate credentials
+			connection.login("username", "password");
+
+			// Get the user's roster
+			Roster roster = connection.getRoster();
+
+			// Print the number of contacts
+			System.out.println("Number of contacts: " + roster.getEntryCount());
+
+			// Enumerate all contacts in the user's roster
+			for (RosterEntry entry : roster.getEntries()) {
+				System.out.println("User: " + entry.getUser());
+			}
+		} catch (XMPPException e) {
+			// Do something better than this!
+			e.printStackTrace();
+		}
+
+	}
+
+}
